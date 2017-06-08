52c9b2b2b7a96611f6bf65636b9e3e6c8db432d5
zhang <981257406@qq.com>
2017-06-08 13:07:23 +0800

    Create OutContainerStartApplication.java
    修改别字

--- src/main/java/com/imooc/controller/OutContainerStartApplication.java
+++ src/main/java/com/imooc/controller/OutContainerStartApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
 /*
- *  如果需要打成war放到外部服务器使用,需要一下步骤:
- * 1、排除tomcat插件
+ *  如果需要打成war放到外部服务器使用,需要以下步骤:
+ * 1、pom中排除tomcat插件
  * 2、创建继承SpringBootServletInitializer的类并重写configure方法,添加@SpringBootApplication
- * 3、最最坑的一点,第二部创建的类必须和controller放到同一个包
+ * 3、最最坑的一点,第二步创建的类必须和controller放到同一个包
  */
 @SpringBootApplication
 public class OutContainerStartApplication extends SpringBootServletInitializer {
